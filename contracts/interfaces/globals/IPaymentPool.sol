052455c2d8f65a8af3422bbbc47ec09ce43bd8a9
Benjamin Mandervelde <benjaminmanderveldedev@protonmail.com>
2022-05-14 18:31:18 +0200

    feat(global): add IPaymentPool and ITokenFactory interfaces

---
+++ contracts/interfaces/globals/IPaymentPool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+import '@openzeppelin/contracts/utils/introspection/IERC165.sol';
+import '@openzeppelin/contracts/token/ERC777/IERC777Sender.sol';
+import '@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol';
+
+interface IPaymentPool is IERC165, IERC777Sender, IERC777Recipient {
+  function lockLiquidity(address from, uint256 amount) external;
+
+  function unlockLiquidity(address from, uint256 amount) external;
+}
